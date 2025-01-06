fd55944fc250e6a08940002c04e92bb90cc1be31
Anjor24 <anjorankle17@gmail.com>
2025-01-06 18:18:47 +0530

    Remove all files from the repository

--- android/app/src/main/kotlin/com/example/neosao_t/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.neosao_t
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/example/speedometer/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.speedometer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
